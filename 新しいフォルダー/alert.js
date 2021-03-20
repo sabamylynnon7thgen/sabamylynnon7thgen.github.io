f721a3f43f9fc61276f42c70a50db2d82ec2986c
sabamylynnon7thgen <78082353+sabamylynnon7thgen@users.noreply.github.com>
2021-03-20 17:24:05 +0900

    Add files via upload

---
+++ 新しいフォルダー/alert.js
@@ -0,0 +1,48 @@
+function a1(){
+    alert('主のTwitterのフォロワーの人数:693\nフォロー中の人数:781')
+}
+function a2(){
+    alert('チャンネル登録してクレメンス')
+}
+function a3(){
+    var hk=Math.floor(Math.random()*13)
+    if(hk==0){
+        alert('Core i7-4790K\n4C8T\n4.0GHz\n4.4GHz\n88w\nHaswell(DevilsCanyon製品群)\nXP2000さんのメイン機に使われたCPU.\n7700Kが出るまでは最もクロックが高いCPUだった')
+    }
+    if(hk==1){
+        alert('Ryzen 9 3900X\n12C24T\n3.8GHz\n4.6GHz\n105w\nZen2(Matisse製品群)\nかのG氏のメイン機やXP2000さんのPC-8801改PCに使われている\n280mm簡易水冷推奨とかいうバケモノ')
+    }
+    if(hk==2){
+        alert('Core i7-8565U\n4C8T\n1.8GHz\n4.6GHz\n15w\nWhiskeyLake-U\nチューニング次第では大きく化けるCPU\n8550Uからターボは大幅にクロックアップ')
+    }
+    if(hk==3){
+        alert('Corei5-9400F\n6C6T\n2.9GHz\n4.1GHz\n65w\nCoffeeLakeReflesh-S\nintelが狂ったような値段で売り出したintelらしくないiGPU無効のCPU\n性能はRyzen 5 3500と同等であるがゲーミングにおいては圧倒的なつよさを持つintelらしい一面も見える')
+    }
+    if(hk==4){
+        alert('Ryzen 5 3600\n6C12T\n3.6GHz\n4.2GHz\n65w\nZen2(Matisse製品群)\n2019年7月7日発売モデルの中では最廉価なモデルだが格上のCore i7-8700Kよりも性能が高い(R15 nT1500cbオーバー)ことや8700Kよりもエコであるので完全に8700Kにトドメを刺したCPUである\nコスパは2019年のCPUの中では最も高い')
+    }
+    if(hk==5){
+        alert('Core i5-10400\n6C12T\n2.9GHz\n4.3GHz\n65w\nCometLake-S\n中３くんが自作するときにつかったCPU\ni5にHTが有効化されている(全モデル有効は初)、のでさらに強くなった\ni7-8700よりも早い\nRyzenの存在って大きいね')
+    }
+    if(hk==6){
+        alert('Core i7-8700K\n6C12T\n3.70GHz\n4.70GHz\n95w\nCoffeeLake-S\nとうとうメインストリームのi7が6コアになりました(Sandy-Eなどエンスージアスト向けには既にある)\nコア数が増えたにもかかわらずTDPは7700Kと比べて4wしか増えていません！\nこのCPUの1個うえに8086Kがあり定格、最大ともに300MHz上昇している')
+    }
+    if(hk==7){
+        alert('Ryzen 7 3700X\n8C16T\n3.6GHz\n4.4GHz\n65w\nZen2(Matisse製品群)\nCore i9-9900Kと同等性能ながらCinebench R15 nTをぶん回している際の消費電力は147wとかなりエコである\nとてもコスパのいいCPUなのでツイ主も欲しいと思っています')
+    }
+    if(hk==8){
+        alert('Ryzen 7 4700U\n8C8T\n2.0GHz\n4.1GHz\n15w\nZen2(Renoir製品群)\nRyzen 7のモバイル版\nintel遅れてると思わせるようなコア数(8Cores)とZen2の高い電力効率で異次元の性能です')
+    }
+    if(hk==9){
+        alert('Core i7-2600K\n4C8T\n3.4GHz\n3.8GHz\n95w\nSandyBridge\nSandy Bridgeマイクロアーキテクチャとか言う10年前の製品です\nこいつはとんでもないほどOC耐性が高く5GHzは朝飯前のレベルです。\nなお発売当時はものすごい円高の影響でi7なのに2.9万台で買えたそうです')
+    }
+    if(hk==10){
+        alert('Core i3-7100U\n2C4T\n2.4GHz\nTBなし\n15w\nKabyLake-U\n次のノート用i3(8130U)にあっさりと負けてしまうCPU(8130U以降はTB有効)\nKabyLakeでRyzenにボロ負けしたからしかたないなぁ\niGPUの動作クロックは最大1GHz!!')
+    }
+    if(hk==11){
+        alert('Core i7-1165G7\n4C8T\n2.8GHz\n4.7GHz\nTDP flexible Range\nTigerLake-U\nintel反撃ののろしをあげたCPU\nintel独自の技術で生み出された10nmプロセスのi7のノート用に仕上がった感じ\n最大動作クロックが10nm i7では初めて4.5GHzを超えたのである')
+    }
+    if(hk==12){
+        alert('Ryzen 7 ExtremeEdition\n8C16T\n1.8GHz\n4.2GHz\n15w\nZen2(Renoir製品群)\nNECのLaVie N15の最上位モデルのみに搭載されたド変態\nベースになったCPUはRyzen 7 4800Uではないかとされる\n本当に15wの冷却装備で冷やしきれるのかが懸念材料となる。')
+    }
+}
